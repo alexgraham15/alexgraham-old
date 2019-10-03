3e660db90cb8671a18528048a2d4b1a1da5dca2b
Alex Graham <Alex@elliottandwragg.local>
2019-10-03 16:07:44 +0100

    Front Page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ function Hero() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1 id="nameAnimation">
-            <span id="firstname">Alex</span> <span id="lastnameHolder"><span id="lastname">Graham</span></span>
+          <h1 class="overlay">
+            <span>Alex Graham</span>
           </h1>
         <p>
-          IT Contractor and Website Builder
+          IT Contractor and Full Stack Developer
         </p>
+        <svg class="mouse scroll-link" xmlns="..." viewBox="0 0 76 130" preserveAspectRatio="xMidYmid meet">
+          <g fill="none" fill-rule="evenodd">
+            <rect width="70" height="118" x="1.5" y="1.5" stroke="#FFF" stroke-width="3" rx="36"/>
+            <circle class="scroll" cx="36.5" cy="31.5" r="4.5" fill="#FFF"/>
+          </g>
+        </svg>
       </header>
     </div>
   );
